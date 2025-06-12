41a730abd58deb403a3ba1a32d86f0c520319e39
itc-s25009 <s25009@std.it-college.ac.jp>
2025-06-12 04:54:56 +0000

    P123 第8章チャレンジ問題終了

---
+++ Chapter08/challenge8-01.py
@@ -0,0 +1,10 @@
+import statistics
+
+nums=[1,33,55,66,23,45,55,22,35]
+print(statistics.stdev(nums))
+
+print(statistics.mean(nums))
+
+print(statistics.median(nums))
+
+print(statistics.mode(nums))
---
+++ Chapter08/challenge8-02.py
@@ -0,0 +1,4 @@
+import cubed
+
+result = cubed.f(3)
+print(result)
